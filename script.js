4c6bfd91e457adad22e8f1d9877961a6f06a7d2b
MD. RAIHAN MAHAMUD <raihan.mahamud@selise.ch>
2022-11-02 12:03:35 +0600

    feature b  done

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ function SID(year, dept, roll) {
 var li1 = document.createElement("li");
 li1.innerHTML = Name + " - " + SID("22", "312", "007");
 document.body.appendChild(li1);
+
+console.log("I'm Developer B");
